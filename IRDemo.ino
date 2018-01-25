9d9a2d90448a4666b2b08262dc79bbc3f33c7527
loopypup <loopypup@gmail.com>
2018-01-24 21:11:15 -0500

    starting work on IR communication

---
+++ IRDemo.ino
@@ -0,0 +1,14 @@
+//This is a test IR communication circuit
+int ledPin = 5;
+int sensorPin = 3;
+int sensorValue = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Hello World");
+}
+
+void loop()
+{
+
+}
