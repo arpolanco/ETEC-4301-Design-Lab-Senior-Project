2f8707fcb1cfb1ac79b36addda61e5861f34335c
garretbs <garretbs@gmail.com>
2018-02-15 00:20:42 +0000

    should just be client.py

--- Communication/client.py
+++ Communication/client.py
@@ -1,13 +1,29 @@
 import socket
+import time
+import picamera
+import io
+import struct
 
-def run():
-	address = "206.21.94.100" #hardcoded as per our design. may need adjusted for testing
+try:
+	address = "192.168.0.4" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
 	host = socket.socket()
-	host.connect((address, port))
-	host.send(open("temp.jpg", "rb").read())
+	host.connect((address, port))	
+	camera = picamera.PiCamera()
+	camera.resolution = (640, 480)
+	camera.start_preview()
+	time.sleep(2) #camera warm-up time
+	buffer = host.makefile('wb')
+	stream = io.BytesIO()
+	for carlos in camera.capture_continuous(stream, 'jpeg'):
+		buffer.write(struct.pack('<L', stream.tell()))
+		buffer.flush()
+		stream.seek(0)
+		buffer.write(stream.read())
+		stream.seek(0)
+		stream.truncate()
+		time.sleep(0.1)
+	buffer.write(struct.pack('<L', 0))
+finally:
+	buffer.close()
 	host.close()
-
-if (__name__ == "__main__"):
-	run()
-	print("Sent temp.jpg to server")
