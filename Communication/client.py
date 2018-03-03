026c6c962918f0ceccda270a0ce57d0a86eed091
garretbs <garretbs@gmail.com>
2018-03-03 00:47:23 +0000

    picamera video stream

--- Communication/client.py
+++ Communication/client.py
@@ -3,27 +3,25 @@ import time
 import picamera
 import io
 import struct
+import threading
 
 try:
 	address = "192.168.0.4" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
 	host = socket.socket()
-	host.connect((address, port))	
+	host.connect((address, port))
 	camera = picamera.PiCamera()
+	camera.framerate = 30
+	#camera.color_effects = (128, 128)
 	camera.resolution = (640, 480)
-	camera.start_preview()
+	#camera.start_preview()
 	time.sleep(2) #camera warm-up time
 	buffer = host.makefile('wb')
-	stream = io.BytesIO()
-	for carlos in camera.capture_continuous(stream, 'jpeg'):
-		buffer.write(struct.pack('<L', stream.tell()))
-		buffer.flush()
-		stream.seek(0)
-		buffer.write(stream.read())
-		stream.seek(0)
-		stream.truncate()
-		time.sleep(0.1)
-	buffer.write(struct.pack('<L', 0))
-finally:
-	buffer.close()
-	host.close()
+	camera.start_recording(buffer, format='h264')
+	camera.wait_recording(100) #todo: either find a way to record indefinitely
+	camera.stop_recording()
+except KeyboardInterrupt:
+	print("dead")
+#finally:
+#	buffer.close()
+#	host.close()
