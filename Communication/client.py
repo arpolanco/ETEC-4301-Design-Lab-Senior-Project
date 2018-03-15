7fb95ed9c2c9bf3065e06effa8942adc1907f415
garretbs <garretbs@gmail.com>
2018-03-15 01:03:07 +0000

    Stream no longer terminates after finite time

--- Communication/client.py
+++ Communication/client.py
@@ -6,19 +6,21 @@ import struct
 import threading
 
 try:
-	address = "192.168.0.4" #hardcoded as per our design. may need adjusted for testing
+	address = "206.21.94.218" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
 	host = socket.socket()
 	host.connect((address, port))
 	camera = picamera.PiCamera()
 	camera.framerate = 30
 	#camera.color_effects = (128, 128)
-	camera.resolution = (640, 480)
+	camera.resolution = (640, 480) #1280x720 results in unacceptable latency
 	#camera.start_preview()
 	time.sleep(2) #camera warm-up time
 	buffer = host.makefile('wb')
 	camera.start_recording(buffer, format='h264')
-	camera.wait_recording(100) #todo: either find a way to record indefinitely
+	camera.wait_recording() #todo: either find a way to record indefinitely
+	while True:
+		time.sleep(5)
 	camera.stop_recording()
 except KeyboardInterrupt:
 	print("dead")
