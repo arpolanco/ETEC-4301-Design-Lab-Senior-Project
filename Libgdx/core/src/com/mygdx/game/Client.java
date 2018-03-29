f8f5879d712594bfda10b990c661efb82c57af76
GarretBS <garretbs@gmail.com>
2018-03-28 22:08:27 -0400

    exit

--- Libgdx/core/src/com/mygdx/game/Client.java
+++ Libgdx/core/src/com/mygdx/game/Client.java
@@ -1,14 +1,20 @@
 package com.mygdx.game;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net.Protocol;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.net.Socket;
+import com.badlogic.gdx.net.SocketHints;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.Socket;
+//import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /*
 import com.badlogic.gdx.Gdx;
@@ -22,27 +28,35 @@ public class Client {
     private InputStream input;
     private Pixmap frame;
     private byte[] buffer;
-    //Protocol protocol;
-    //SocketHints hints;
+    private Image droneFrame;
+    Protocol protocol;
+    SocketHints hints;
     
-    final String HOST = "206.21.94.161";
+    final String HOST = "206.21.94.82";
     final int PORT = 1101;
     
     public Client() throws IOException{
         //initialize socket
-        //socket = Gdx.net.newClientSocket(protocol, HOST, PORT, hints);
-        //this gives an error, but why?
-        socket = new Socket(HOST, PORT);
+        hints = new SocketHints();
+        hints.connectTimeout = 0;
+        hints.socketTimeout = 0;
+        hints.keepAlive = true;
+        socket = Gdx.net.newClientSocket(protocol, HOST, PORT, hints);
+        //socket = new Socket(HOST, PORT);
         input = socket.getInputStream();
         buffer = new byte[640 * 480 * 5];
         //shout type to server
     }
     
     public Texture getImage(){
-        int bytesRead;
         try {
-            bytesRead = input.read(buffer);
-            frame = new Pixmap(buffer, 0, bytesRead);
+            //maybe this sends a request to the server, which then returns the
+            //current frame when it was called
+            int bytesRead = -1;
+            //while((bytesRead = input.read(buffer)) > 0){
+            //}
+            //frame = new Pixmap(buffer, 0, bytesRead);
+            input.skip(500000000);
         } catch (IOException ex) {
             System.exit(-1);
         }
