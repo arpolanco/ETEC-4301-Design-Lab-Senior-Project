29dd22c88125f03e114b48b7e35ff01853fddbb0
garretbs <garretbs@gmail.com>
2018-03-30 19:21:21 +0000

    Disabled pi video streaming

--- Communication/client.py
+++ Communication/client.py
@@ -6,22 +6,20 @@ import struct
 import threading
 
 try:
-	address = "206.21.94.218" #hardcoded as per our design. may need adjusted for testing
+	address = "206.21.94.253" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
 	host = socket.socket()
 	host.connect((address, port))
 	camera = picamera.PiCamera()
 	camera.framerate = 30
-	#camera.color_effects = (128, 128)
 	camera.resolution = (640, 480) #1280x720 results in unacceptable latency
-	#camera.start_preview()
 	time.sleep(2) #camera warm-up time
 	buffer = host.makefile('wb')
-	camera.start_recording(buffer, format='h264')
-	camera.wait_recording() #todo: either find a way to record indefinitely
+	#camera.start_recording(buffer, format='h264')
+	#camera.wait_recording() #todo: either find a way to record indefinitely
 	while True:
 		time.sleep(5)
-	camera.stop_recording()
+	#camera.stop_recording()
 except KeyboardInterrupt:
 	print("dead")
 #finally:
