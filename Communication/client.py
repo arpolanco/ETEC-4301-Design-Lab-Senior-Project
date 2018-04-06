56bdba6fee2780a82d464a99941ffb2c90c1ab7a
garretbs <garretbs@gmail.com>
2018-04-06 19:11:30 +0000

    Drone client signals that it is a drone

--- Communication/client.py
+++ Communication/client.py
@@ -6,20 +6,25 @@ import struct
 import threading
 
 try:
-	address = "206.21.94.238" #hardcoded as per our design. may need adjusted for testing
+	address = "206.21.94.200" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
 	host = socket.socket()
 	host.connect((address, port))
-	camera = picamera.PiCamera()
-	camera.framerate = 30
-	camera.resolution = (640, 480) #1280x720 results in unacceptable latency
-	time.sleep(2) #camera warm-up time
-	buffer = host.makefile('wb')
+	#camera = picamera.PiCamera()
+	#camera.framerate = 30
+	#camera.resolution = (640, 480) #1280x720 results in unacceptable latency
+	#time.sleep(2) #camera warm-up time
+	buffer = host.makefile('w')
 	#camera.start_recording(buffer, format='h264')
 	#camera.wait_recording() #todo: either find a way to record indefinitely
+	buffer.write("DRONE\n")
+	buffer.flush()
 	while True:
-               #host.send("drone")
-                pass
+        	data = host.recv(1)
+		buffer.write(data)
+		buffer.write('\n')
+		buffer.flush()
+                #pass
 		#time.sleep(5)
 	#camera.stop_recording()
 except KeyboardInterrupt:
