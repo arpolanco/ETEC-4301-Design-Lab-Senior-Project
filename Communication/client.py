5d7985ddca701bd7cf00c58379eccd57b3a16344
GarretBS <garretbs@gmail.com>
2018-04-20 13:48:12 -0400

    Pi client now receives telemetry from server

--- Communication/client.py
+++ Communication/client.py
@@ -1,32 +1,29 @@
 import socket
-import time
-import picamera
 import io
-import struct
-import threading
 
 try:
-	address = "206.21.94.58" #hardcoded as per our design. may need adjusted for testing
+	address = "206.21.94.201" #hardcoded as per our design. may need adjusted for testing
 	port = 1101
-	host = socket.socket()
-	host.connect((address, port))
+	server = socket.socket()
+	server.connect((address, port))
 	#camera = picamera.PiCamera()
 	#camera.framerate = 30
 	#camera.color_effects = (128, 128)
 	#camera.resolution = (640, 480) #1280x720 results in unacceptable latency
 	#camera.start_preview()
 	#time.sleep(2) #camera warm-up time
-	buffer = host.makefile('wb')
+	buffer = server.makefile('w')
 	buffer.write("DRONE\n")
 	buffer.flush()
 	#camera.start_recording(buffer, format='h264')
 	#camera.wait_recording() #todo: either find a way to record indefinitely
 	while True:
-		pass
+		telemetry = server.recv(1) #blocking
+		#print(format(telemetry, "8b"))
 		#time.sleep(5)
 	camera.stop_recording()
 except KeyboardInterrupt:
 	print("dead")
 #finally:
 #	buffer.close()
-#	host.close()
+#	server.close()
